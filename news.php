fedd8fafff4813132743b40a8e782cff7b859281
Kento <rogeraabbccdd@users.noreply.github.com>
2018-06-05 08:11:21 +0800

    Update news.php

--- news.php
+++ news.php
@@ -82,7 +82,7 @@
 						?>
     </div>
 	                </div>
-                <div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+                <div id="altt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
                     	<script>
 						$(".ssaa li").hover(
 							function ()
